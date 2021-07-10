1a471508c22195543bb755a303e1206b96fbb15e
oxdc <29519076+oxdc@users.noreply.github.com>
2021-07-10 10:40:37 +0800

    delete old files

--- jsonify_html/__init__.py
+++
@@ -1,4 +0,0 @@
-import jsonify_html.cmd.buildin_cmd
-from .command_manager import register_command
-from .exceptions import *
-from .parser import load_package, load_template, from_package, from_template, TemplatePackage
--- jsonify_html/parser/__init__.py
+++
@@ -1,3 +0,0 @@
-from .selector import parse_selector
-from .template import load_package, load_template, from_template, from_package, TemplatePackage, \
-    parse_template, parse_use_statement
